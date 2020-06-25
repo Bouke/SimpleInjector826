52b862efa450e66d3f3343a082cf46803ed05903
Bouke Haarsma <bhaarsma@cloudbilling.nl>
2020-06-25 21:07:01 +0200

    ❌ Inject dependency into view page (not working)

--- SimpleInjector826/MyDependency.cs
+++ SimpleInjector826/MyDependency.cs
@@ -2,5 +2,9 @@
 {
     public class MyDependency
     {
+        public override string ToString()
+        {
+            return "I am the dependency";
+        }
     }
 }
--- SimpleInjector826/MyWebViewPage.cs
+++ SimpleInjector826/MyWebViewPage.cs
@@ -4,9 +4,13 @@ namespace SimpleInjector826
 {
     public abstract class MyWebViewPage : WebViewPage
     {
+        [Import]
+        public MyDependency MyDependency { get; set; }
     }
 
     public abstract class MyWebViewPage<T> : WebViewPage<T>
     {
+        [Import]
+        public MyDependency MyDependency { get; set; }
     }
 }
