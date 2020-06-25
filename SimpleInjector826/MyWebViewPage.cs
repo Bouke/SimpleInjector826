037552c520127aef110f949f04ee7feb4e159fc8
Bouke Haarsma <bhaarsma@cloudbilling.nl>
2020-06-25 21:02:53 +0200

    Configure custom view page

---
+++ SimpleInjector826/MyWebViewPage.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SimpleInjector826
+{
+    public abstract class MyWebViewPage : WebViewPage
+    {
+    }
+
+    public abstract class MyWebViewPage<T> : WebViewPage<T>
+    {
+    }
+}
